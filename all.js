9bcaae58bc1c77a669ea9989cdcb3ac715ed90b3
Emily Chen <wanchichen88@gmail.com>
2020-07-09 16:15:11 +0800

    修改甜度資料

--- all.js
+++ all.js
@@ -7,32 +7,17 @@ var app = new Vue({
                 category: '水果', //商品分類 - string
                 content: '濃郁的香氣及滑順的口感', //商品敘述 - string
                 description: '果皮鮮紅', //商品說明 - string
-                sugar: 13,
-                sugarValue: '65%',
+                // sugar: 13,
+                // sugarValue: '65%',
                 imageUrl: 'https://images.unsplash.com/photo-1587486936739-78df7470c7e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60', //商品圖片 - string
                 enabled: 'true', //是否上架 - boolean
                 origin_price: 790, //原價 - number(integer)
                 price: 600, //售價 - number(integer)
                 unit: '斤', //單位 - string
-                options: {
-                    
-                    comments: [
-                        {
-                            id:1,
-                            stars: 5,
-                            name: '小明',
-                            avator: 'https://images.unsplash.com/photo-1586297098710-0382a496c814?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
-                            comment: '香甜好吃'
-                        },
-                        {   
-                            id:2,
-                            stars: 4,
-                            name: '結倫',
-                            avator: 'https://images.unsplash.com/photo-1586297098710-0382a496c814?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
-                            comment: '普通'
-                        },
-                    ]
-                }
+                sugar: {
+                    value: 13,
+                    percent: '65%'
+                },
 
             },
             {
@@ -41,27 +26,21 @@ var app = new Vue({
                 category: '水果',
                 content: '清爽的香氣及Q彈的口感',
                 description: '果皮金黃',
-                sugar: 15,
-                sugarValue: '75%',
+                // sugar: 15,
+                // sugarValue: '75%',
                 imageUrl: 'https://images.unsplash.com/photo-1560452460-10e6cffc2153?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
                 enabled: 'true',
                 origin_price: 600,
                 price: 450,
                 unit: '斤',
-                options: {
-                    comments: [{
-                        id:1,
-                        stars: 5,
-                        name: '小美',
-                        avator: 'https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
-                        comment: '自製芒果冰好選擇'
-                    }]
-                }
+                sugar: {
+                    value: 15,
+                    percent: '75%'
+                },
 
             }
         ],
         tempData: [], //暫存要處理的商品資料
-        tempComment: [], //暫存心得資料
     },
     methods: {
         openModal(action, item) {
@@ -71,15 +50,12 @@ var app = new Vue({
                     $('#productModal').modal('show');
                     break;
                 case 'edit':
-                    this.tempData = Object.assign({}, item); //如果是編輯狀態，就新增一個空物件，將item丟進去做處理，然後賦值給this.tempData
+                    //this.tempData = Object.assign({}, item); //如果是編輯狀態，就新增一個空物件，將item丟進去做處理，然後賦值給this.tempData
+                    this.tempData = JSON.parse(JSON.stringify(item));//改用深拷貝
                     $('#productModal').modal('show');
                     break;
                 case 'delete':
                     $('#deleteModal').modal('show');
-                    this.tempData = Object.assign({}, item);
-                    break;
-                case 'message':
-                    $('#messageModal').modal('show');
                     this.tempData = Object.assign({}, item);
                     break;
                 default:
@@ -106,15 +82,15 @@ var app = new Vue({
                 })
             } else { //沒有，就新增push
                 const id = new Date().getTime();
-                let sugar = this.tempData.sugar;
-
+                let sugar = this.tempData.sugar.value;
                 function percent(sugar) {
                     let total = 20;
                     let num = Math.round(sugar / total * 100) + "%";
                     return num //整數百分比
                 };
                 let value = percent(sugar);
-                this.tempData.sugarValue = value;
+                this.tempData.sugar.percent = value;
+                console.log(this.tempData.sugar);
                 this.tempData.id = id;
                 this.products.push(this.tempData);
 
